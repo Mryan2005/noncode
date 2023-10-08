077ab0787210a1d01514c6f5e071ca3eec39e28d
Mryan2005 <A2564011261@163.com>
2023-10-08 09:13:53 +0800

    add(main.py)

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import sys
 
-# init values
-step = []
-tab = 0
-tabs = []
-position = 0 # 0 就是本位的意思
+# 初始化变量
+step = []  # 存储每一步的代码
+tab = 0  # 缩进级别
+tabs = []  # 存储每一步的缩进级别
+position = 0  # 当前位置
 
 print("欢迎使用noncode")
 print("请开始输入")
 
-
+# 添加起始步骤
 step.append("start")
 while True:
     text = input("")
 
-    # command
-    if ':back' in text:
+    # 处理命令
+    if ':back' in text:  # 回退命令
         try:
             position = position + int(text[5:])
         except ValueError:
             position = position + 1
         finally:
             continue
-    elif ':wq' in text:
+    elif ':wq' in text:  # 保存并退出命令
         end = len(step)-1
         step.remove(step[end])
         try:
@@ -41,15 +41,15 @@ while True:
                 file.write(i+'\n')
         break
 
-    # check input
-    if position >= 1:
+    # 检查输入
+    if position >= 1:  # 替换指定位置的代码
         needChangePosition  = len(step) - position
         step[needChangePosition] = '    '*tabs[needChangePosition-1]+text
         position = 0
-    elif "if:" in text  or "while:" in text:
+    elif "if:" in text  or "while:" in text:  # 处理if和while语句
         step.append('   '*tab+text)
         tabs.append(tab)
         tab = tab + 1
-    else:
+    else:  # 添加普通代码
         step.append('   '*tab+text)
         tabs.append(tab)
